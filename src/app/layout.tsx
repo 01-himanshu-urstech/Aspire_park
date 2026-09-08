bc8d49e0733a707bce52956e80bdc27b6f61d87b
Himanshu Bisht <himanshu.urstechsolution@gmail.com>
2026-09-08 11:25:31 +0530

    google tag manager initialized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Aspire Centurian Park by Gaurs",
@@ -7,10 +8,16 @@ export const metadata: Metadata = {
     "Aspire Centurian Park by Gaurs offers grand luxury residences in Greater Noida (W) with 3 & 4 BHK apartments, premium amenities, and a 30:70 payment plan.",
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
+
+      <GoogleTagManager gtmId="GTM-KWT9F8PS" />
     </html>
   );
 }
